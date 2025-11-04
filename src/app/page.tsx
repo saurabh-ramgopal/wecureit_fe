44814230a2434bc151905b8822e1fa823b3cab10
Prakruthi19 <prakruthi.koteshwar@gmail.com>
2025-11-04 14:18:44 -0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,5 @@
 "use client";
-import AdminLoginPage from './(public)/patient/login/page';
-import PatientLoginPage from './(public)/patient/login/page';
-import DoctorLoginPage from './(public)/doctor/login/page';
 import HomeView from './(public)/home/page';
-import { useState, useEffect } from 'react';
 
 export default function Page() {
   return (
